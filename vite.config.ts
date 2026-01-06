4ae9842b0f62ca07c379e8406399e66423cf3fd6
SikandarJODD <sikandar6174@gmail.com>
2026-01-07 03:06:12 +0530

    minor changes in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,29 +3,29 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()],
-	optimizeDeps: {
-		force: false,
-		exclude: ["@lucide/svelte"],
-	},
-	resolve: {
-		dedupe: ["svelte"],
-	},
-	server: {
-		fs: {
-			strict: false,
-		},
-		watch: {
-			ignored: ["**/node_modules/**", "**/.svelte-kit/**"],
-		},
-	},
-	build: {
-		cssCodeSplit: true,
-		rollupOptions: {
-			maxParallelFileOps: 2,
-		},
-	},
-	// ssr: {
+	plugins: [tailwindcss(),sveltekit()],
+	// optimizeDeps: {
+	// 	force: false,
+	// 	exclude: ["@lucide/svelte"],
+	// },
+	// resolve: {
+	// 	dedupe: ["svelte"],
+	// },
+	// server: {
+	// 	fs: {
+	// 		strict: false,
+	// 	},
+	// 	watch: {
+	// 		ignored: ["**/node_modules/**", "**/.svelte-kit/**"],
+	// 	},
+	// },
+	// build: {
+	// 	cssCodeSplit: true,
+	// 	rollupOptions: {
+	// 		maxParallelFileOps: 2,
+	// 	},
+	// },
+	// // ssr: {
 	// 	noExternal: ['shiki', '@shikijs/langs', '@shikijs/themes']
 	// }
 });
