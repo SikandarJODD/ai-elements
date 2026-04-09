4892c1e9b2d030a65920c36135d257d29fbc1340
SikandarJODD <sikandar6174@gmail.com>
2026-04-10 01:35:36 +0530

    feat: improved submit, basic example

---
+++ src/routes/(main)/components/prompt-input/examples/basic.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import * as PromptInput from "$lib/components/ai-elements/prompt-input";
+	import type { Message } from "$lib/components/ai-elements/prompt-input";
+
+	import { Chat } from "@ai-sdk/svelte";
+	let chat = new Chat({});
+
+	function handleSubmit(message: Message) {
+		chat.sendMessage({
+			text: message.text,
+			files: message.files,
+		});
+	}
+</script>
+
+<PromptInput.Root class="w-xl" onSubmit={handleSubmit}>
+	<PromptInput.Body>
+		<PromptInput.Textarea />
+	</PromptInput.Body>
+	<PromptInput.Toolbar class="justify-end">
+		<PromptInput.Submit status={chat.status} />
+	</PromptInput.Toolbar>
+</PromptInput.Root>
